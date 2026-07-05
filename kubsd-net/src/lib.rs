de6ca70c7b2f587812b70454db638d324be60d19
LeBaronDeCharlus <git@lebaron.sh>
2026-07-05 17:38:07 +0200

    Add kubsd-net crate: NetManager trait and FakeNetManager

---
+++ kubsd-net/src/lib.rs
@@ -0,0 +1,11 @@
+pub mod error;
+pub mod fake;
+
+pub use error::NetError;
+pub use fake::FakeNetManager;
+
+pub trait NetManager {
+    fn ensure_bridge_exists(&self, bridge: &str) -> Result<(), NetError>;
+    fn attach_jail(&self, jail_name: &str, bridge: &str, epair_base: &str, address: &str) -> Result<(), NetError>;
+    fn detach_jail(&self, epair_base: &str) -> Result<(), NetError>;
+}
