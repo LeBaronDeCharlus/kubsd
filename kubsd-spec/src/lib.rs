e4e0d05d559784085f12ce674d2ab2ef09460934
LeBaronDeCharlus <git@lebaron.sh>
2026-07-05 13:53:42 +0200

    Add cpu/memory resource string parsing

--- kubsd-spec/src/lib.rs
+++ kubsd-spec/src/lib.rs
@@ -1,6 +1,8 @@
 pub mod error;
+pub mod resources;
 pub mod types;
 pub mod validate;
 
 pub use error::SpecError;
+pub use resources::{cores_to_pcpu_percent, parse_cpu_cores, parse_memory_bytes};
 pub use types::{JailSpec, Metadata, NetworkSpec, RestartPolicy, ResourcesSpec, Spec};
